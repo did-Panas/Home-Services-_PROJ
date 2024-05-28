f7685e192c73f83d713083d858cbbcca72c36285
Oleg Bashylov <olegbash94@gmail.com>
2024-05-28 17:59:20 +0300

    reviews-section_DONE

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -8,7 +8,7 @@
 // При необхідності підключаємо додаткові модулі слайдера, вказуючи їх у {} через кому
 // Приклад: { Navigation, Autoplay }
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 /*
 Основні модулі слайдера:
 Navigation, Pagination, Autoplay, 
@@ -33,12 +33,12 @@ function initSliders() {
 		new Swiper('.reviews__slider', { // Вказуємо склас потрібного слайдера
 			// Підключаємо модулі слайдера
 			// для конкретного випадку
-			modules: [Navigation],
+			modules: [Navigation, Pagination],
 			observer: true,
 			observeParents: true,
 			slidesPerView: 1,
 			spaceBetween: 0,
-			//autoHeight: true,
+			autoHeight: true,
 			speed: 800,
 
 			//touchRatio: 0,
@@ -57,12 +57,12 @@ function initSliders() {
 			*/
 
 			// Пагінація
-			/*
+
 			pagination: {
-				el: '.swiper-pagination',
+				el: '.reviews__bullets',
 				clickable: true,
 			},
-			*/
+
 
 			// Скроллбар
 			/*
@@ -74,8 +74,8 @@ function initSliders() {
 
 			// Кнопки "вліво/вправо"
 			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
+				prevEl: '.reviews__arrow--left',
+				nextEl: '.reviews__arrow--right',
 			},
 			/*
 			// Брейкпоінти
